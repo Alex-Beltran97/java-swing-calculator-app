30ea54d262e10f3899940a0265d001ba43f8ec5a
Pedro Beltran <pebeltranhe@gmail.com>
2024-06-03 01:38:11 -0500

    Adding events for type numbers and symbols and event for clean screen

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Calculator extends JFrame {
     private JPanel panelApp;
@@ -19,8 +21,11 @@ public class Calculator extends JFrame {
     private JButton a9Btn;
     private JButton btnEquals;
     private JButton CEBtn;
-    private JButton a0Button;
+    private JButton a0Btn;
     private JPanel controllers;
+    private JButton aCommaBtn;
+    private JButton CBtn;
+    private String text;
 
     public Calculator() {
         setContentPane(panelApp);
@@ -29,10 +34,123 @@ public class Calculator extends JFrame {
         setBounds(600, 400, 250,270);
         setLocationRelativeTo(null);
         setVisible(true);
-         setResizable(false);
+        setResizable(false);
+
+        // Button Events
+        handleBtnEvents();
+
+        // Cleaner button
+        CEBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                text = "0";
+                screen.setText(text);
+            }
+        });
     }
 
     public static void main(String[] args) {
         new Calculator();
+    }
+
+    public void handleBtnEvents() {
+        // Number buttons
+        a1Btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleScreenText(a1Btn.getText());
+            }
+        });
+        a2Btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleScreenText(a2Btn.getText());
+            }
+        });
+        a3Btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleScreenText(a3Btn.getText());
+            }
+        });
+        a4Btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleScreenText(a4Btn.getText());
+            }
+        });
+        a5Btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleScreenText(a5Btn.getText());
+            }
+        });
+        a6Btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleScreenText(a6Btn.getText());
+            }
+        });
+        a7Btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleScreenText(a7Btn.getText());
+            }
+        });
+        a8Btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleScreenText(a8Btn.getText());
+            }
+        });
+        a9Btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleScreenText(a9Btn.getText());
+            }
+        });
+        a0Btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleScreenText(a0Btn.getText());
+            }
+        });
+
+        // Symbol buttons
+
+        btnPlus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleScreenText(btnPlus.getText());
+            }
+        });
+        btnSubs.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleScreenText(btnSubs.getText());
+            }
+        });
+        btnMulti.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleScreenText(btnMulti.getText());
+            }
+        });
+        btnDiv.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleScreenText(btnDiv.getText());
+            }
+        });
+    }
+
+    public void handleScreenText(String txt) {
+        if (screen.getText().equals("0")) {
+            text = txt;
+        } else {
+            text += txt;
+        }
+
+        screen.setText(text);
     }
 }
